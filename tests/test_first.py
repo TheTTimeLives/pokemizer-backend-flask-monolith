08902b59f659e1d69089363b64f34d6301f10441
Taylor M <melnikprod@gmail.com>
2023-03-04 14:43:16 -0500

    Fixed testing tests

--- tests/test_first.py
+++ tests/test_first.py
@@ -1,6 +1,8 @@
-from unittest import TestCase
+import unittest
 import json
 import pprint
+from unittest import TestCase
+
 
 # run with python -m unitttest file_name.py for just this file
 # run with python -m unittest discover to discover all files prefixed with test and run them
@@ -17,7 +19,8 @@ whatever_data = [
     }
 ]
 
+# preface with test and it should run
 class MyTest(TestCase):
-    def some_test(self):
+    def test_some(self):
         # run some logic stuff
         self.assertEqual(len(whatever_data), 3)
